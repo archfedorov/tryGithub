662e46a2267868cf8b0cba9b4c6354e83f740080
archilan.fedorov <archilan.fedorov@mytona.com>
2020-02-13 23:46:36 +0900

    init?

---
+++ SFML_TTRY/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+        static int counter = 0;
+        counter++;
+        std::cout << counter << std::endl;
+    }
+
+    return 0;
+}
